1983a6f3587f81c46ac7e976ab4fb6c1c6272711
nikhil-ghosh-berkeley <nikhil_ghosh@berkeley.edu>
2023-08-12 04:55:58 +0000

    added fix for padding

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -457,7 +457,6 @@ class LlamaModel(LlamaPreTrainedModel):
     def set_input_embeddings(self, value):
         self.embed_tokens = value
 
-    # Copied from transformers.models.bart.modeling_bart.BartDecoder._prepare_decoder_attention_mask
     def _prepare_decoder_attention_mask(self, attention_mask, input_shape, inputs_embeds, past_key_values_length):
         # create causal mask
         # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
@@ -479,6 +478,20 @@ class LlamaModel(LlamaPreTrainedModel):
                 expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
             )
 
+        combined_attention_mask = torch.clamp_min(combined_attention_mask, torch.finfo(inputs_embeds.dtype).min)
+        # for every prompt instance, examine if there is any token who has a full masking attention tensor
+        fully_masked = (combined_attention_mask == torch.finfo(inputs_embeds.dtype).min).all(dim=-1)
+        if torch.any(fully_masked):
+            # This can happen when your prompts are left padded and you want to masked out the left padded tokens
+            # However the operation 'expanded_attn_mask + combined_attention_mask' above will cause those left padded tokens
+            # to have a fully masked attention tensor, and cause numerical instability(inf) during inference. Therefore
+            # for those tokens who has attention tensor fully masked, force them to at least attend to itself.
+            indices = torch.arange(
+                combined_attention_mask.size(2) + past_key_values_length, device=combined_attention_mask.device
+            )
+            indices = indices.unsqueeze(0).unsqueeze(0).expand_as(fully_masked)
+            # Set the n-th elements in the n-th tensors to be zero
+            combined_attention_mask[fully_masked, indices[fully_masked]] = 0.0
         return combined_attention_mask
 
     @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
